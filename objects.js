60a3656f7358aa057eaaf52edd3aa838a705af34
JBarbas <jbarbascubero@gmail.com>
2019-02-26 02:22:57 +0100

    ¿Buscais problemas?
    Animación de pokeball cerrandose y abriendose hecha y lista para dominar el mundo

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-function decoration(x, y, image) {
+function Decoration(x, y, image) {
     this.x = x;
     this.y = y;    
     this.img = image;
@@ -8,16 +8,92 @@ function decoration(x, y, image) {
     }
 }
 
-function button(x, y, normalImage, hoverImage, width, height) {
+//Pokeball hereda de Decoration
+function Pokeball(x, y, image, direction) {
+	Decoration.call(this, x, y, image);
+	this.speed = 0
+	this.dir = direction;
+	this.cycleDone = true;
+	this.countdown = 15;
+	//Mueve la pokeball a la derecha hasta que llegue al centro, después la devuelve a su posición
+	this.rightAndBack = function() {
+		if (this.dir === "right") {			
+			if (this.x >= 398) {
+				this.x = 398;
+				this.speed = 0;
+				this.dir = "left";
+			}
+			else {
+				this.speed += 0.5;
+				this.x += this.speed;
+			}
+		}
+		else {	
+			if (this.countdown > 0)	{ //cuando llega al centro se para durante una fracción de segundo
+				this.countdown--;
+			}
+			else if (this.x <= 15) {
+				this.x = 15;
+				this.speed = 0;
+				this.dir = "right";
+				this.cycleDone = true;
+				inAnimation = false;
+				this.countdown = 15;
+			}
+			else {
+				this.speed -= 0.5;
+				this.x += this.speed;
+			}
+		}
+		ctx = pokedex.context;
+		ctx.clearRect(0, 0, this.x + 295, this.y + 600); //Se borra la parte que cubre la pokeball
+	}
+	//Mueve la pokeball a la izquierda hasta que llegue al centro, después la devuelve a su posición
+	this.leftAndBack = function() {
+		if (this.dir === "left") {			
+			if (this.x <= 692) {
+				this.x = 692;
+				this.speed = 0;
+				this.dir = "right";
+			}
+			else {
+				this.speed -= 0.5;
+				this.x += this.speed;
+			}
+		}
+		else {			
+			if (this.countdown > 0)	{ //cuando llega al centro se para durante una fracción de segundo
+				this.countdown--;
+			}
+			else if (this.x >= 1075) {
+				this.x = 1075;
+				this.speed = 0;
+				this.dir = "left";
+				this.cycleDone = true;
+				inAnimation = false;
+				this.countdown = 15;
+			}
+			else {
+				this.speed += 0.5;
+				this.x += this.speed;
+			}
+		}
+		ctx = pokedex.context;
+		ctx.clearRect(this.x + 10, this.y, pokedex.canvas.width, pokedex.canvas.height); //Se borra la parte que cubre la pokeball
+	}
+}
+
+function Button(x, y, normalImage, hoverImage, width, height) {
     this.x = x;
     this.y = y;    
     this.normal = normalImage;
     this.hover = hoverImage;
     this.img = normalImage;
     this.width = width;
-    this.height = height;
+    this.height = height;    
     this.update = function() {
         ctx = pokedex.context;
+        //Button Hover
         if (pokedex.mouseX > this.x && pokedex.mouseX < this.x + this.width && pokedex.mouseY > this.y && pokedex.mouseY < this.y + this.height) {
         	this.img = hoverImage;
         }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var images = new Array();
+//Se cargan todas las imagenes antes de crear el canvas
 function preload() {
     var latch = preload.arguments.length
     for (i = 0; i < preload.arguments.length; i++) {
@@ -22,19 +23,25 @@ preload(
     "img/PokedexHover.png"
 )
 
+//Variables globales
 var pokeball_izq;
 var pokeball_der;
 var background;
 var listaPokemon;
 
+//Locks and Flags
+var inAnimation = false;
+
+//inicialización de variables
 function startPokedex() {
-    pokeball_izq = new decoration(15, 200, images[1]);
-    pokeball_der = new decoration(1075, 200, images[2]);
-    background = new decoration(255, 230, images[3]);
-    listaPokemon = new button(300, 400, images[4], images[5], 235, 251);
+    pokeball_izq = new Pokeball(15, 200, images[1], "right"); //Se posiciona a la izquierda pero mira hacia la derecha
+    pokeball_der = new Pokeball(1075, 200, images[2], "left"); //Se posiciona a la derecha pero mira a la izquierda
+    background = new Decoration(255, 230, images[3]);
+    listaPokemon = new Button(300, 400, images[4], images[5], 235, 251);
     pokedex.start();
 }
 
+//Gestion del canvas
 var pokedex = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -44,8 +51,21 @@ var pokedex = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updatePokedex, 20);
         window.addEventListener('mousemove', function (e) {
-          pokedex.mouseX = e.pageX;
-          pokedex.mouseY = e.pageY;
+            pokedex.mouseX = e.pageX;
+            pokedex.mouseY = e.pageY;
+        });
+        window.addEventListener('click', function(e) {
+            pokedex.clickX = e.pageX;
+            pokedex.clickY = e.pageY;
+
+            if (!inAnimation) {
+            //ListaPokemon onClick
+                if (pokedex.clickX > listaPokemon.x && pokedex.clickX < listaPokemon.x + listaPokemon.width && pokedex.clickY > listaPokemon.y && pokedex.clickY < listaPokemon.y + listaPokemon.height) {
+                    pokeball_izq.cycleDone = false;
+                    pokeball_der.cycleDone = false;
+                    inAnimation = true;
+                }
+            }        
         });
     },
     clear : function() {
@@ -53,11 +73,18 @@ var pokedex = {
     }
 }
 
+//Actualizar canvas
 function updatePokedex() {
     pokedex.clear();   
     background.update();
+    listaPokemon.update();
+    if (!pokeball_izq.cycleDone) {
+        pokeball_izq.rightAndBack();
+    }
+    if (!pokeball_der.cycleDone) {
+        pokeball_der.leftAndBack();
+    }
     pokeball_izq.update();
     pokeball_der.update();
-    listaPokemon.update();
 }
     
