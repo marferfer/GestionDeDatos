cecbf293899bb7548af63267e3d8b9b61284f38b
Mario Fernández <mariofdezfdez@hotmail.com>
2019-03-07 13:03:29 +0100

    Capturados metapods
    Solo agregado unas lineas que no sirven de nada pero si sirven para empezar a hacer el upload de imagenes en Añadir

--- script.js
+++ script.js
@@ -217,7 +217,22 @@ var pokedex = {
                     switch (pokeState.name) {
                         case "listMenu":                            
                             break;
-                        case "addMenu":                            
+                        case "addMenu":
+                            if (pokedex.clickX > btnAddFoto.x + btnAddFoto.group.x && pokedex.clickX < btnAddFoto.x + btnAddFoto.group.x + btnAddFoto.width && pokedex.clickY > btnAddFoto.y + btnAddFoto.group.y && pokedex.clickY < btnAddFoto.y + btnAddFoto.group.y + btnAddFoto.height) {
+                               var preview = document.querySelector('img'); //selects the query named img
+                               var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+                               var reader  = new FileReader();
+
+                               reader.onloadend = function () {
+                                   preview.src = reader.result;
+                               }
+
+                               if (file) {
+                                   reader.readAsDataURL(file); //reads the data as a URL
+                               } else {
+                                   preview.src = "";
+                               }
+                            }                            
                             break;
                         case "bddMenu":  
                             if (pokedex.clickX > mongodb.x + mongodb.group.x && pokedex.clickX < mongodb.x + mongodb.group.x + mongodb.width && pokedex.clickY > mongodb.y + mongodb.group.y && pokedex.clickY < mongodb.y + mongodb.group.y + mongodb.height) {
